4daf7519428184d3e0a08ced8951bf13bc3d0deb
Natan Mustapic <natan.mustapic95@gmail.com>
2024-03-25 17:11:07 +0100

    first commit

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using Ispit.Model;
+using Ispit.Model.Klase;
+
+namespace Ispit.Konzola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Podaci podaci = new Podaci();
+            
+
+            // Grupiranje milijunaša po banci
+            var GrupirajPremaBanci = from k in podaci.ListaKlijenata
+                                     where k.Stanje >= 1000000
+                                     group k.ImePrezime by k.Banka into g
+                                     select new GrupiraniMilijunasi
+                                     {
+                                         Banka = g.Key,
+                                         Milijunasi = g.ToList()
+                                     };
+
+            Console.WriteLine("Grupiranje milijunaša po banci:");
+            foreach (var grupa in GrupirajPremaBanci)
+            {
+                Console.Write($"{grupa.Banka}: ");
+                foreach (var milijunas in grupa.Milijunasi)
+                {
+                    Console.Write($" {milijunas}");
+                }
+                Console.Write("\n");
+            }
+
+
+            // Lista milijunaša
+            var IzvjestajMilijunasa = from k in podaci.ListaKlijenata
+                                      join b in podaci.ListaBanki on k.Banka equals b.Simbol
+                                      where k.Stanje >= 1000000
+                                      select new { ImePrezime = k.ImePrezime, Banka = b.Naziv };
+
+            Console.WriteLine("\nIspis milijunaša i banke u kojoj se nalazi:");
+            foreach (var milijunas in IzvjestajMilijunasa)
+            {
+                Console.WriteLine($"{milijunas.ImePrezime} je u {milijunas.Banka}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ispit.Model/Podaci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Podaci
+    {
+        public List<Klase.Banka> ListaBanki { get; set; }
+        public List<Klase.Klijent> ListaKlijenata { get; set; }
+
+        public Podaci()
+        {
+            // Inicijalizacija liste banaka
+            ListaBanki = new List<Klase.Banka>
+            {
+                new Klase.Banka { Simbol = "PBZ", Naziv = "Privredna Banka Zagreb" },
+                new Klase.Banka { Simbol = "OTP", Naziv = "OTP Banka" },
+                new Klase.Banka { Simbol = "ERSTE", Naziv = " Erste&Steiermärkische Bank" }
+            };
+
+            // Inicijalizacija liste klijenata
+            ListaKlijenata = new List<Klase.Klijent>
+            {
+                new Klase.Klijent { ImePrezime = "Nikola Tesla", Stanje = 50000, Banka = "PBZ" },
+                new Klase.Klijent { ImePrezime = "Ruđer Bošković", Stanje = 1000000, Banka = "PBZ" },
+                new Klase.Klijent { ImePrezime = "Marin Getaldić", Stanje = 2000000, Banka = "OTP" },
+                new Klase.Klijent { ImePrezime = "Ivan Supek", Stanje = 30000, Banka = "OTP" },
+                new Klase.Klijent { ImePrezime = "Miroslav Krleža", Stanje = 1500000, Banka = "OTP" },
+                new Klase.Klijent { ImePrezime = "Vesna Parun", Stanje = 200000, Banka = "ERSTE" },
+                new Klase.Klijent { ImePrezime = "Ivo Andrić", Stanje = 80000, Banka = "ERSTE" },
+                new Klase.Klijent { ImePrezime = "Dubravko Horvatić", Stanje = 5000000, Banka = "ERSTE" },
+                new Klase.Klijent { ImePrezime = "Antun Gustav Matoš", Stanje = 700, Banka = "PBZ" },
+                new Klase.Klijent { ImePrezime = "Marija Jurić Zagorka", Stanje = 200000, Banka = "PBZ" }
+            };
+        }
+    }
+}
